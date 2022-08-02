afc910cbef05ec70d55361d829fcefc32fb0c2ad
Jake Derkowski <77984636+JakeD-5Q@users.noreply.github.com>
2022-08-02 10:11:53 -0500

    Update deployLighthouse.ps1

--- Azure Lighthouse/5QTemplates/deployLighthouse.ps1
+++ Azure Lighthouse/5QTemplates/deployLighthouse.ps1
@@ -1,9 +1,17 @@
+param(
+    [Parameter(Mandatory = $true)]$location
+)
+
 $location = 'centralus'
-$DeploymentName = 'LightHouse'
+$Resource = 'LightHouse'
 $TemplateFile = 'lighthouse.template.json'
 $ParameterFile = 'lighthouse.parameters.json'
 
 
+$today = Get-Date -Format "MM-dd-yyyy"
+$suffix = Get-Random -Maximum 100
+$deploySuffix = $today + "_$suffix"
+$DeploymentName = $deploySuffix + "_$Resource"
 
 New-AzDeployment -Name $DeploymentName `
     -Location $location `
